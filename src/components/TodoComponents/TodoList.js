d17745cb98cb9ae479b6d1fbb0ea5498dc6a6566
Danielle B <danibaxx@hotmail.com>
2019-10-02 19:12:48 -0600

    added reactstrap/bootstrap dependencies for styling, added <TodoList /> to App.js, TodoList.js is taking in data from data.js and rendering the list of data, TodoForm.js is holding the form information with Reactstrap, Todo is taking in props to display the tasks, functionality of buttons coming next

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,50 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react';
+import TodoForm from './TodoForm';
+import Todo from './Todo';
+import data from '../../data';
+
+class TodoList extends React.Component {
+  constructor() {
+    super();
+    this.state= {
+      tasks: data
+    }
+  }
+
+  toggleTask = (event, taskId) => {
+    this.setState({
+      tasks: this.state.tasks.map(task => {
+        if (task.id === taskId) {
+          console.log("TodoList Task", task)
+          return {
+            ...task,
+            completed: !task.completed
+          };
+        } else {
+          return task;
+        }
+      })
+    });
+  };
+
+  render() {
+    return(
+      <div>
+        <div className='todo-list'>
+          {this.state.tasks.map(task => (
+            <Todo 
+              key={task.id}
+              tasks={task}
+              onClick={event => this.toggleTask(event, task.id)}
+            />
+          ))}
+            <TodoForm addTask={this.addTask}/>
+        </div>
+      </div>
+    );
+  }
+};
+
+export default TodoList;
